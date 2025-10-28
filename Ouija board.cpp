7a25ae9d8ea541d542e0ce2c9f869ae2a822f5af
M1T0-0x00 <cyber.null0x00@gmail.com>
2025-10-28 16:57:14 +0300

    Add Ouija board.cpp file

---
+++ Ouija board.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// communicate with dead people
+int main() {
+ std :: cout << "----------------------------------\n";
+ std :: cout <<"|          x YES   NO x          |\n";
+ std :: cout <<"|                                |\n";
+ std :: cout <<"|         ABCDEFGHIJKLM          |\n";
+ std :: cout <<"|         NOPQRSTUVWXYZ          |\n";
+ std :: cout <<"|                                |\n";
+ std :: cout <<"|           1234567890           |\n";
+ std :: cout <<"---------------------------------- ";
+}
