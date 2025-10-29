bd5e7d1f183cbe7eb859da779acc2001e1b6b2bd
M1T0-0x00 <cyber.null0x00@gmail.com>
2025-10-29 16:15:05 +0300

    Create Fever_test.cpp

---
+++ Fever_test.cpp
@@ -0,0 +1,17 @@
+// Fever temp test
+#include <iostream>
+
+int main() {
+
+double Temp;
+
+std :: cout << "Enter your temp : ";
+std :: cin >> Temp;
+
+if (Temp > 37){
+  std :: cout << "\nYou have a fever";
+}
+ else{
+   std :: cout <<"\nYou don't have a fever\n";
+ }
+}
